a51e4deb4663f52cb0f0345ea2a8f704c52a8987
Jair Velazquez Reyes <jairreyes689@gmail.com>
2024-12-02 15:30:26 -0600

    aristeo

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -10,15 +10,13 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   final EmailAuth emailAuth = EmailAuth();
-
   final TextEditingController emailController = TextEditingController();
-
   final TextEditingController passwordController = TextEditingController();
   final GitHubAuth githubAuth = GitHubAuth();
   final GoogleAuth googleAuth = GoogleAuth();
 
   bool _obscureText = true;
-  // Variable para controlar la visibilidad de la contraseña
+
   void _togglePasswordVisibility() {
     setState(() {
       _obscureText = !_obscureText;
@@ -27,41 +25,44 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       body: Center(
         child: SingleChildScrollView(
-          padding: EdgeInsets.symmetric(horizontal: 24),
+          padding: EdgeInsets.symmetric(horizontal: size.width * 0.08),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Image.asset('assets/logo.png',
-                  height: 120), // Logo en la parte superior
-              SizedBox(height: 30),
+              Image.asset(
+                'assets/logo.png',
+                height: size.height * 0.15,
+              ),
+              SizedBox(height: size.height * 0.03),
               Text(
                 "Bienvenido a GAMESTECH!",
                 textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                style: TextStyle(
+                  fontSize: size.width * 0.06,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              SizedBox(height: 20),
-              // Campo de correo electrónico
+              SizedBox(height: size.height * 0.02),
               TextField(
                 controller: emailController,
                 decoration: InputDecoration(
                   hintText: "Enter your email",
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(color: Colors.black),
                   ),
                   focusedBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(
-                        color: Colors.blue), // Borde azul cuando está enfocado
+                    borderSide: BorderSide(color: Colors.blue),
                   ),
-                  filled: false,
                 ),
               ),
-              SizedBox(height: 15),
-              // Campo de contraseña
+              SizedBox(height: size.height * 0.02),
               TextField(
                 controller: passwordController,
                 obscureText: _obscureText,
@@ -69,22 +70,20 @@ class _LoginScreenState extends State<LoginScreen> {
                   hintText: "Enter your password",
                   suffixIcon: IconButton(
                     icon: Icon(
-                        _obscureText ? Icons.visibility : Icons.visibility_off),
+                      _obscureText ? Icons.visibility : Icons.visibility_off,
+                    ),
                     onPressed: _togglePasswordVisibility,
                   ),
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.black),
                   ),
                   focusedBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10),
                     borderSide: BorderSide(color: Colors.blue),
                   ),
-                  filled: false,
                 ),
               ),
-              SizedBox(height: 20),
-              // Botón de inicio de sesión
+              SizedBox(height: size.height * 0.03),
               ElevatedButton(
                 onPressed: () async {
                   String email = emailController.text.trim();
@@ -96,28 +95,32 @@ class _LoginScreenState extends State<LoginScreen> {
                   if (isLoggedIn) {
                     Navigator.pushReplacementNamed(context, '/home');
                   } else {
-                    // Mostrar un mensaje de error si el inicio de sesión falla
                     ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text('Verificar contraseña o correo.')),
+                      SnackBar(
+                        content: Text('Verificar contraseña o correo.'),
+                      ),
                     );
                   }
                 },
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue, // Cambiar el color si lo deseas
+                  backgroundColor: Colors.blue,
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10),
                   ),
-
-                  minimumSize: Size(double.infinity, 50), // Ancho completo
+                  padding: EdgeInsets.symmetric(
+                    vertical: size.height * 0.015,
+                    horizontal: size.width * 0.3,
+                  ),
                 ),
                 child: Text(
                   "Login",
-                  style: TextStyle(color: Colors.white),
+                  style: TextStyle(
+                      fontSize: size.width * 0.05,
+                      color: Colors.white
+                      ),
                 ),
               ),
-
-              SizedBox(height: 20),
-              // Texto de registro
+              SizedBox(height: size.height * 0.02),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -132,7 +135,7 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                 ],
               ),
-              SizedBox(height: 20),
+              SizedBox(height: size.height * 0.02),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -147,21 +150,18 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                 ],
               ),
-              SizedBox(height: 20),
+              SizedBox(height: size.height * 0.02),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text("O Inicia sesion con: "),
+                  Text("O inicia sesión con: "),
                   IconButton(
                     icon: Icon(Icons.g_mobiledata, color: Colors.red),
                     onPressed: () async {
-                      // Llamada para el inicio de sesión con Google
                       final user = await googleAuth.signInWithGoogle();
                       if (user != null) {
-                        // Navegar a la pantalla principal o dashboard
                         Navigator.pushReplacementNamed(context, '/home');
                       } else {
-                        // Mostrar un mensaje de error si falla el inicio de sesión
                         ScaffoldMessenger.of(context).showSnackBar(
                           SnackBar(
                               content: Text('Failed to sign in with Google')),
@@ -172,18 +172,14 @@ class _LoginScreenState extends State<LoginScreen> {
                   IconButton(
                     onPressed: () async {
                       final user = await githubAuth.signInWithGitHub();
-
-                      // Aquí puedes implementar la autenticación con GitHub
                       if (user != null) {
                         Navigator.pushReplacementNamed(context, '/home');
-
-                        Navigator.pop(
-                            context); // Regresar a la pantalla principal o dashboard
                       } else {
                         ScaffoldMessenger.of(context).showSnackBar(
                           SnackBar(
-                              content:
-                                  Text('Falló el inicio de sesión con GitHub')),
+                            content:
+                                Text('Falló el inicio de sesión con GitHub'),
+                          ),
                         );
                       }
                     },
